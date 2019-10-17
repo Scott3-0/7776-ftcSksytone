e52c1b42fe74ce0f62885e736dd00bcd50cbeed5
Daniel <dholland7257@student.pps.net>
2019-10-16 20:40:29 -0700

    "hard math"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Daniel1StickTranslateTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Daniel1StickTranslateTeleOp.java
@@ -7,13 +7,30 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode._Libs.hardware.SkystoneHardware;
+
+import java.lang.Math;
 
 @TeleOp(name="1StickTranslate-Daniel")
 public class Daniel1StickTranslateTeleOp extends OpMode {
+    SkystoneHardware robot = new SkystoneHardware(); //get the hardware
+    DcMotor motors[];
+
+    float power=0;
+    float theta=0;
+
     @Override
     public void init(){
         telemetry.addData(">","Initializing 1StickTranslate");
-        SkystoneHardware robot = new SkystoneHardware();
+        robot.init(hardwareMap); //idk what this does -daniel
+
+        motors = new DcMotor[4];
+        motors[0]=robot.fr;
+        motors[1]=robot.fl;
+        motors[2] = robot.bl;
+        motors[3] = robot.br;
     }
 
     @Override
@@ -23,6 +40,8 @@ public class Daniel1StickTranslateTeleOp extends OpMode {
 
     @Override
     public void loop(){
-        telemetry.addData(">","Loop");
+        //telemetry.addData(">","Loop");
+        power = (float) Math.sqrt((gamepad1.left_stick_x*gamepad1.left_stick_x)+(gamepad1.left_stick_y*gamepad1.left_stick_y)); //sqrt(x^2+y^2)= distance of joystick to origin use as power for motors !!!check output values before sending to robot!!!
+        theta = (float) Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x); //atan(y/x)= angle (in rad) of joystick from origin use to determine direction of robot movement !!!also check!!!
     }
 }
